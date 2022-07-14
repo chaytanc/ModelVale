6f1e1ff8a353a4baffc047ea4400da7e615c0511
Chaytan Inman <35582442+chaytanc@users.noreply.github.com>
2022-07-14 16:03:03 -0700

    Setup Health Bar components

---
+++ ModelVale/ModelVale/CustomComponents/HealthBar.h
@@ -0,0 +1,16 @@
+//
+//  HealthBar.h
+//  ModelVale
+//
+//  Created by Chaytan Inman on 7/14/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HealthBar : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ModelVale/ModelVale/CustomComponents/HealthBar.m
@@ -0,0 +1,24 @@
+//
+//  HealthBar.m
+//  ModelVale
+//
+//  Created by Chaytan Inman on 7/14/22.
+//
+
+#import "HealthBar.h"
+
+@interface HealthBar()
+@property (nonatomic, strong) CAShapeLayer* shapeLayer;
+@end
+
+@implementation HealthBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
